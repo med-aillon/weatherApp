9578efbf88ac6e654cb08e1f2080d2fa30b7133f
med-aillon <mehdimerghichi@gmail.com>
2023-09-19 13:52:18 +0100

    Faire une requête de base

---
+++ script.js
@@ -0,0 +1,20 @@
+const loader = document.querySelector('.loader-container');
+const errorInformation = document.querySelector('.error-informations');
+
+async function getWeatherData() {
+  try {
+    const response = await fetch(
+      'http://api.airvisual.com/v2/nearest_city?key=API_KEY'
+    );
+
+    const responseData = await response.json();
+    const sortedData = {
+      city: responseData.data.city,
+      country: responseData.data.country,
+      temperature: responseData.data.current.weather.tp,
+      iconId: responseData.data.current.weather.ic,
+    };
+    console.log(sortedData);
+  } catch (error) {}
+}
+getWeatherData();
